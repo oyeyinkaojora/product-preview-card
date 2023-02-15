a918b80bedbb12752c65101706857878a5128a97
oyeyinkaojora <oyeyinkaojoro@gmail.com>
2023-02-15 17:33:40 +0100

    accesibility fix

---
+++ script.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        darkCyan: "hsl(158, 36%, 37%)",
+        cream: "hsl(30, 38%, 92%)",
+        veryDarkBlue: "hsl(212, 21%, 14%)",
+        darkGrayishBlue: "hsl(228, 12%, 48%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+        serif: ["Fraunces", "serif"],
+      },
+    },
+  },
+};
